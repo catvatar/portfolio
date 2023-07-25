b27aec135db7e66ab716c7b12e4ddc5323142196
catvatar <olekgogol@protonmail.com>
2023-07-25 22:50:14 +0200

    More temporary code while learning

---
+++ nextjs-blog/components/date.js
@@ -0,0 +1,6 @@
+import { parseISO, format } from 'date-fns';
+
+export default function Date({ dateString }) {
+  const date = parseISO(dateString);
+  return <time dateTime={dateString}>{format(date, 'ddiii_MM_yy')}</time>;
+}
