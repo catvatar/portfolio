ad6302a359dbc915b500e7be283e2d11a4e4c1a4
catvatar <olekgogol@protonmail.com>
2023-09-25 09:37:27 +0200

    added cypress and updated git ignore

---
+++ cypress/e2e/tags.cy.ts
@@ -0,0 +1,5 @@
+describe("Tag selection", () => {
+  it("passes", () => {
+    cy.visit("https://example.cypress.io");
+  });
+});
--- src/ui/side-navigation.tsx
+++ src/ui/side-navigation.tsx
@@ -6,6 +6,9 @@ import SideNavigationigationWrapper from "../components/side-navigation-wrapper"
 import { getSortedPostsData } from "../lib/posts";
 import Break from "../components/break";
 
+// Dodaj coc jak otwarty jest artykul
+//pixel w menu na telefonie
+
 export default function SideNavigation() {
   const postsObjects = {
     portfolio: getSortedPostsData("portfolio"),
