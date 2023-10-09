e655bd0ef70c86a094a7fe952a4d6d65bfad324a
Olek Gogol <olekgogol@gmail.com>
2023-10-09 17:59:46 +0200

    refactoring

--- src/lib/Context.tsx
+++ src/lib/Context.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useSearchParams } from "next/navigation";
+// import { useSearchParams } from "next/navigation";
 import { createContext, useState } from "react";
 
 export const tagsContext = createContext<any>(null);
@@ -24,15 +24,16 @@ export function TagsContextProvider({ children, allPostsData }) {
   const uniqueTags = getUniqueTags(allPostsData);
 
   const tagsStateObject = (() => {
-    const intermediateTags = useSearchParams()?.get("tags");
-    const clickedTagsFromSearchParams = intermediateTags
-      ? intermediateTags
-          .replace(/[\[\]]/g, "")
-          .split(",")
-          .filter((elem) => {
-            return uniqueTags.includes(elem);
-          })
-      : [];
+    // const intermediateTags = useSearchParams()?.get("tags");
+    // const clickedTagsFromSearchParams = intermediateTags
+    //   ? intermediateTags
+    //       .replace(/[\[\]]/g, "")
+    //       .split(",")
+    //       .filter((elem) => {
+    //         return uniqueTags.includes(elem);
+    //       })
+    //   : [];
+    const clickedTagsFromSearchParams = [];
     return useState<string[]>(clickedTagsFromSearchParams);
   })();
 
