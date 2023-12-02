0bac93712c0fe5cdba5e0f3c3dad41300193b219
catvatar <olekgogol@protonmail.com>
2023-12-02 11:24:20 +0100

    working build after refactoring

--- src/components/ui/top-navigation.tsx
+++ src/components/ui/top-navigation.tsx
@@ -8,8 +8,8 @@ export default function TopNavigation() {
   return (
     <TopNavigationWrapper>
       <div className="p-4 px-8">
-        <div className="flex h-14 w-auto flex-row flex-nowrap items-stretch rounded-full bg-primary-300 shadow-xl">
-          <div className="rounded-l-full border-8 border-primary-500 p-2 px-4">
+        <div className="flex h-14 w-auto flex-row flex-nowrap items-stretch rounded-xl bg-primary-300 shadow-xl">
+          <div className="rounded-l-xl border-8 border-primary-500 p-2 px-4">
             <div>Aleksander Gogol</div>
           </div>
           <BackElement>
@@ -25,7 +25,7 @@ export default function TopNavigation() {
             }
           >
             <MenuButton>
-              <div className="rounded-r-full border-8 border-l-0 border-primary-500 p-2 px-8">
+              <div className="rounded-r-xl border-8 border-l-0 border-primary-500 p-2 px-8">
                 Menu
               </div>
             </MenuButton>
